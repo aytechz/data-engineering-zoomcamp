1498551103d21ad9bfc81c0e8ea5cba8e1698bfc
aytechz <fmaytekin@gmail.com>
2024-01-27 23:39:14 -0800

    week1 docker

--- 01-docker-terraform/2_docker_sql/pipeline.py
+++ 01-docker-terraform/2_docker_sql/pipeline.py
@@ -4,8 +4,12 @@ import pandas as pd
 
 print(sys.argv)
 
+# arg 0 is the name of the file
+# arg #1 is whatever user put on
 day = sys.argv[1]
 
 # some fancy stuff with pandas
+# docker run -it test:pandas 2024-01-21
+# run docker w/ date arg
 
 print(f'job finished successfully for day = {day}')
